02d77726555edd74c6d3d97cc1a741b1fde6c5b0
neoenox <neoenox@gmail.com>
2026-09-25 18:54:28 +0900

    fix: harden driving search input and upstream failures (#13)
    * fix: fail safely on upstream API errors
    
    * fix: make Wikipedia summary optional on API failure
    
    * fix: validate driving search distance through form
    
    * fix: handle invalid searches and empty destination data
    
    * fix: parse DEBUG as boolean
    
    * ci: use Django 4 compatible Python versions
    
    * test: cover form and upstream failure handling
    
    * fix: render driving form validation errors
    
    * test: avoid locale-specific distance error text

--- driving/forms.py
+++ driving/forms.py
@@ -1,14 +1,31 @@
-from turtle import distance
 from django import forms
-from django.forms import ModelForm
-
-from driving.models import Src
 
 
 class SrcForm(forms.Form):
     """出発地のフォーム"""
-    src = forms.CharField(label='出発地', max_length=255, required=True,
-                          widget=forms.TextInput(
-                              attrs={'placeholder': '名称、座標どちらでも可', 'class': 'form-control col-xl'}))
-    distance = forms.IntegerField(label='所要時間', required=True, widget=forms.TextInput(
-        attrs={'type': 'number', 'class': 'form-control col-3', 'list': 'distanceList', 'value': '30', 'min': '1'}))
+
+    src = forms.CharField(
+        label="出発地",
+        max_length=255,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "名称、座標どちらでも可",
+                "class": "form-control col-xl",
+            }
+        ),
+    )
+    distance = forms.IntegerField(
+        label="所要時間",
+        required=True,
+        min_value=1,
+        widget=forms.TextInput(
+            attrs={
+                "type": "number",
+                "class": "form-control col-3",
+                "list": "distanceList",
+                "value": "30",
+                "min": "1",
+            }
+        ),
+    )
