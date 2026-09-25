6c772bdd9b42f2884a8f8263da91a11549750913
neoenox <neoenox@gmail.com>
2026-09-25 18:29:38 +0900

    fix: parse DEBUG as boolean

--- weekenddestpy/settings.py
+++ weekenddestpy/settings.py
@@ -24,7 +24,7 @@ env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 
 # Application definition
