1250bf66f56896a4c8ec58789ffb5ffb39f22a99
neoenox <neoenox@gmail.com>
2026-09-25 18:29:34 +0900

    fix: handle invalid searches and empty destination data

--- driving/views.py
+++ driving/views.py
@@ -1,83 +1,114 @@
-import logging
-from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-from driving.forms import SrcForm
-from driving.models import Dest, Src, geoCash, routeCash
-from geopy.distance import geodesic
 import random
 
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from geopy.distance import geodesic
+
+from driving.forms import SrcForm
+from driving.models import Dest, geoCash, routeCash
+from driving.utils.api import ApiError
 from driving.utils.geo import Geo
 from driving.utils.route import Route
 from driving.utils.wiki import Wiki
 
-# Create your views here.
 
-
-@require_http_methods(['GET'])
+@require_http_methods(["GET"])
 def driving_index(request):
     """検索画面"""
 
-    params = {
-        'form': SrcForm(request.GET or None),
-    }
+    form = SrcForm(request.GET or None)
+    if not request.GET:
+        return render(request, "driving/index.html", {"form": form})
 
-    if "src" in request.GET and "distance" in request.GET:
-        # 出発地が設定されている場合
-        srcParam = request.GET.get("src")
-        distanceParam = int(request.GET.get("distance"))
-        src = []
-        # 検索したことがある場所ならcashから取得
+    if not form.is_valid():
+        return render(request, "driving/index.html", {"form": form})
 
-        geocash = geoCash.objects.filter(src=srcParam).first()
-        if geocash is not None:
-            src.append(geocash.latitude)
-            src.append(geocash.longitude)
+    src_param = form.cleaned_data["src"]
+    distance_param = form.cleaned_data["distance"]
+
+    try:
+        src = _resolve_source(src_param)
+
+        candidates = []
+        destinations = list(Dest.objects.all())
+        for destination in destinations:
+            distance = geodesic(
+                (float(src[0]), float(src[1])),
+                (float(destination.latitude), float(destination.longitude)),
+            ).km
+            if distance_param * 0.9 <= distance <= distance_param:
+                candidates.append(destination)
+
+        if not candidates:
+            candidates = destinations
+
+        if not candidates:
+            form.add_error(None, "目的地データがありません。管理者にお問い合わせください。")
+            return render(request, "driving/index.html", {"form": form})
+
+        choice = random.choice(candidates)
+        route_cache = routeCash.objects.filter(
+            src=src[0] + "," + src[1],
+            dest=choice.latitude + "," + choice.longitude,
+        ).first()
+
+        if route_cache is not None:
+            highway = route_cache.highway
+            localway = route_cache.localway
         else:
-            geo = Geo().getGeo(srcParam)
-            src.append(str(geo["result"]["latitude"]))
-            src.append(str(geo["result"]["longitude"]))
-            geoCash.objects.create(
-                src=srcParam, latitude=geo["result"]["latitude"], longitude=geo["result"]["longitude"])
+            route_response = Route().getRoute(
+                [
+                    {
+                        "src": src_param,
+                        "dest": choice.latitude + "," + choice.longitude,
+                        "place_name": choice.name,
+                    }
+                ]
+            )
+            route_item = route_response["result"][0]["distance"]
+            highway = route_item["highway"]
+            localway = route_item["localway"]
+            routeCash.objects.create(
+                src=src[0] + "," + src[1],
+                dest=choice.latitude + "," + choice.longitude,
+                highway=highway,
+                localway=localway,
+            )
 
-        randomDest = []
-        for d in Dest.objects.all():
-            dis = geodesic((float(src[0]), float(src[1])),
-                           (float(d.latitude), float(d.longitude))).km
-            if dis <= distanceParam and dis >= distanceParam * 0.9:
-                randomDest.append(d)
+        wiki_summary = Wiki().getWiki(choice.name)
+        params = {
+            "name": choice.name,
+            "src": src[0] + "," + src[1],
+            "dest": choice.latitude + "," + choice.longitude,
+            "highway": highway,
+            "localway": localway,
+            "wiki": wiki_summary or "",
+        }
+        return render(request, "driving/list.html", params)
+    except (ApiError, KeyError, IndexError, TypeError, ValueError):
+        form.add_error(
+            None,
+            "外部サービスから経路情報を取得できませんでした。時間をおいて再度お試しください。",
+        )
+        return render(request, "driving/index.html", {"form": form})
 
-        if not randomDest:
-            # 空ならすべてからランダムにする
-            for d in Dest.objects.all():
-                randomDest.append(d)
-                
-        # ランダムに１つ選ぶ
-        choiced = random.choice(randomDest)
 
-        routecashmodel = routeCash.objects.filter(
-            src=src[0] + "," + src[1], dest=choiced.latitude + "," + choiced.longitude).first()
+def _resolve_source(src_param):
+    geocash = geoCash.objects.filter(src=src_param).first()
+    if geocash is not None:
+        return [geocash.latitude, geocash.longitude]
 
-        if routecashmodel is not None:
-            params = dict(
-                name=choiced.name, src=src[0] + "," + src[1], dest=choiced.latitude + "," + choiced.longitude, highway=routecashmodel.highway, localway=routecashmodel.localway)
-        else:
-            route = Route().getRoute(
-                [dict(src=srcParam, dest=choiced.latitude + "," + choiced.longitude, place_name=choiced.name)])
-            params = dict(
-                name=choiced.name, src=src[0] + "," + src[1], dest=choiced.latitude + "," + choiced.longitude, highway=route["result"][0]["distance"]["highway"], localway=route["result"][0]["distance"]["localway"])
-            routeCash.objects.create(src=src[0] + "," + src[1], dest=choiced.latitude + "," + choiced.longitude,
-                                     highway=route["result"][0]["distance"]["highway"], localway=route["result"][0]["distance"]["localway"])
+    geo_response = Geo().getGeo(src_param)
+    geo_result = geo_response["result"]
+    latitude = str(geo_result["latitude"])
+    longitude = str(geo_result["longitude"])
+    # Validate that coordinates are numeric before persisting the cache.
+    float(latitude)
+    float(longitude)
 
-        wikiSumally = Wiki().getWiki(choiced.name)
-
-        params["wiki"] = wikiSumally if wikiSumally is not None else ""
-
-        # params = dict(
-        #     name=choiced.name, src=src[0]+ "," + src[1], dest=choiced.latitude + "," + choiced.longitude, highway=100, localway=200)
-
-        return render(request,
-                      'driving/list.html', params)
-    else:
-        # それ以外
-        return render(request,
-                      'driving/index.html', params)
+    geoCash.objects.create(
+        src=src_param,
+        latitude=latitude,
+        longitude=longitude,
+    )
+    return [latitude, longitude]
